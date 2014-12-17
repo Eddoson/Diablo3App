d2dc4f9499396c66e717743a201d459eca4234fd
Eddoson <eddy2by6@gmail.com>
2014-12-16 22:39:45 -0500

    Testing changes to ignore file third  time

--- app/src/main/java/com/example/eddoson/diablo3app/ParseApplication.java
+++ app/src/main/java/com/example/eddoson/diablo3app/ParseApplication.java
@@ -13,7 +13,7 @@ public class ParseApplication extends Application
     @Override
     public void onCreate()
     {
-        //testing change 2
+        //testing change 3
         super.onCreate();
         Parse.initialize(this, "n9bfmaSXweeTz4CmKsQWoiFn8KoMn7c4opPO0sRh", "Txl4b42XMZvWj30yTcoJwUtxHWA4onVTAzPmydR2");
     }
